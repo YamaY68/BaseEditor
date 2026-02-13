0dac6791c5d234153bd9e87b92def527ef5bb494
ASOST\2416043 <y.yuuto68@gmail.com>
2026-02-13 12:12:30 +0900

    コメント

--- BaseEditor/Src/Scene/EditScene/EditScene.cpp
+++ BaseEditor/Src/Scene/EditScene/EditScene.cpp
@@ -17,14 +17,14 @@
 
 #include"../../Object/Actor/Camera/Camera.h"
 
-
-
 EditScene::EditScene(void)
 {
+
 }
 
 EditScene::~EditScene(void)
 {
+
 }
 
 void EditScene::Load(void)
@@ -34,6 +34,7 @@ void EditScene::Load(void)
 
 void EditScene::Init(void)
 {
+
 }
 
 void EditScene::Update(void)
@@ -43,36 +44,42 @@ void EditScene::Update(void)
 
 void EditScene::Draw(void)
 {
+
 }
 
 void EditScene::Release(void)
 {
+
 }
 
 void EditScene::RegisterActorFileNames(void) {
+	//探索するファイルの親ディレクトリ
     std::string rootPath = "Src/Object/Actor";
 
+	//再帰的にディレクトリを探索
     for (const auto& entry : std::filesystem::recursive_directory_iterator(rootPath)) {
 
-        if (entry.is_regular_file() && entry.path().extension() == ".cpp") {
+		//拡張子が.cppのものだけ
+        if (entry.path().extension() == ".cpp") {
 
+			//ディレクトリと拡張子を除いたファイル名
             std::string actorName = entry.path().stem().string();
-
+			//フルパス
             std::string fullPath = entry.path().string();
             
-            //
             //nposは見つからなかったときの値
             //見つからなかったのではない＝見つかった
 			if (fullPath.find("Base") != std::string::npos) {
                 continue;
             }
-
+			//Componentクラスかどうかで分ける
 			if (fullPath.find("Component") != std::string::npos) {
 				componentFileNames_.push_back(actorName);
             }
+			//Actorクラス
             else
             {
-			actorFileNames_.push_back(actorName);
+                actorFileNames_.push_back(actorName);
             }
         }
     }
